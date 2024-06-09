6cd95beacb101aa82958f9890b637eeb7e638e3c
koehmsto <koehmsto@oregonstate.edu>
2024-06-09 15:30:59 -0700

    Update finalgame.ino
    Added switch functionality to turn off and restart the game whenever you are playing. Also added debounce for the switch to make it more reliable.

--- finalgame.ino
+++ finalgame.ino
@@ -1,5 +1,6 @@
 #include <Adafruit_CircuitPlayground.h>
 
+
 int playerScore = 0;
 int gameSpeed = 500;
 int constantPixel;
@@ -20,17 +21,21 @@ void setup() {
   randomSeed(analogRead(A6));  // seeds the random number with the signal from A6
   constantPixel = random(0, 10);
   CircuitPlayground.setPixelColor(constantPixel, 0x00FF00);  // Green color for constant pixel
-  Serial.println("Loop Start setup");
+  Serial.println("Loop Start setu p");
 }
 
 void loop() {
-
-  while (isGameRunning) {  // game is in playing state
-      
+  while (isGameRunning) {    // game is in playing state
+    
     for (int i = 0; i < 10; i++) {  // makes dot go in circle
-      
       int p;
-
+      if(!switchFlag) {
+        CircuitPlayground.clearPixels();
+        missAmount = 0;
+        gameSpeed = 500;
+        isGameRunning = false;
+        break;
+      }
       if (forward)
         p = i;
       else
@@ -48,6 +53,10 @@ void loop() {
       else
         previousPixel = p + 1;
 
+      // Ensure previousPixel is within bounds
+      if (previousPixel < 0) previousPixel = 9;
+      if (previousPixel > 9) previousPixel = 0;
+
       if (previousPixel != constantPixel) {
         CircuitPlayground.setPixelColor(previousPixel, 0x000000);  // Reset previous pixel
       }
@@ -56,24 +65,24 @@ void loop() {
 
       // Check if the button is pressed
       if (buttonInterrupt == true) {
-
         if (p == constantPixel) {
           playerScore++;
 
           Serial.println("Hit!");
-          //Serial.println(playerScore);
+          
           CircuitPlayground.playTone(440, 200);  // Play a sound to indicate a hit
-          //Set a new constant pixel
+          // Set a new constant pixel
           CircuitPlayground.setPixelColor(constantPixel, 0x000000);
           constantPixel = random(0, 10);
           CircuitPlayground.setPixelColor(constantPixel, 0x00FF00);  // Green color for new constant pixel
-          gameSpeed = gameSpeed - 50;                                //increases game speed for each time the green dot is hit
-          if (gameSpeed < 50) {        // end game if speed gets to 0
+          gameSpeed = gameSpeed - 50;                                // increases game speed for each time the green dot is hit
+          if (gameSpeed < 50) {                                      // end game if speed gets to 0
             CircuitPlayground.clearPixels();
             CircuitPlayground.playTone(500, 100); //win tone
             CircuitPlayground.playTone(700, 200);
             CircuitPlayground.playTone(500, 100);
             isGameRunning = false;
+
             CircuitPlayground.clearPixels();
           }
           buttonInterrupt = false;
@@ -83,35 +92,38 @@ void loop() {
           missAmount++;  // increment miss amount
           CircuitPlayground.playTone(200, 200);
           Serial.println("Miss!");
+          Serial.println(missAmount);
           buttonInterrupt = false;
+          if (missAmount > 2) {  // if more than 2 misses, the game will end and execute missedAmount
+            isGameRunning = false;
+            missedAmount();
+            gameSpeed = 500;
+            break;
+          }
         }
-      }
-      /*if(!switchFlag) {
-        CircuitPlayground.clearPixels();
-        gameSpeed = 500;
-         CircuitPlayground.setPixelColor(constantPixel, 0x000000);
-        constantPixel = random(0, 10);
-        CircuitPlayground.setPixelColor(constantPixel, 0x00FF00);  // Green color for constant pixel
-        break;
-
-      } */
-      if (missAmount > 2) {  // if more than 2 misses, the game will end and execute missedAmount
-        isGameRunning = false;
-        missedAmount();
-        gameSpeed = 500;
-        break;
-      }
-    }
-    if (isGameRunning == true) {
-      if (forward && 9 != constantPixel) {
-        CircuitPlayground.setPixelColor(9, 0x000000);
-      } else if (0 != constantPixel) {
-        CircuitPlayground.setPixelColor(0, 0x000000);
       }
     }
   }
-  if (button2Interrupt == true) {
+  if(!switchFlag) { 
+    while (!switchFlag) {
+      delay(100); // Wait for the switch to be toggled back on
+    }
+    // Restart the game
+    missAmount = 0;
+    gameSpeed = 500;
+    isGameRunning = true;
+    constantPixel = random(0, 10); // Update constantPixel when the game restarts
+    CircuitPlayground.setPixelColor(constantPixel, 0x00FF00);  // Green color for constant pixel
+  }
+  if (isGameRunning == true) {
+    if (forward && 9 != constantPixel) {
+      CircuitPlayground.setPixelColor(9, 0x000000); 
+    } else if (0 != constantPixel) {
+      CircuitPlayground.setPixelColor(0, 0x000000);
+    }
+  }
 
+  if (button2Interrupt == true) {
     missAmount = 0;
     gameSpeed = 500;
     Serial.println("Interrupt");
@@ -149,5 +161,14 @@ void button2ISR() {
 }
 
 void switchISR() {
-    switchFlag = !switchFlag; // Toggle the switch state
+  // Delay to debounce the switch
+  delay(50);
+
+  // Read the current state of the switch pin
+  int switchState = digitalRead(7);
+
+  // Update the switchFlag only if the switch state is different
+  if (switchState != switchFlag) {
+    switchFlag = switchState;
+  }
 }
